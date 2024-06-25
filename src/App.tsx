739ecd6d414edfc6cd86046b66d095d87e82da55
OGxLuxuries <ogluxuries@gmail.com>
2024-06-25 15:00:19 -0400

    adsa

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,12 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -28,6 +34,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>Hi there</p>
     </>
   )
 }

bb3c71033f0269607b949ef1fd4952398841ffc9
OG Luxuries <ogluxuries@gmail.com>
2025-06-15 22:12:13 -0400

    feat(landing-page): display “Under Construction” notice and cooler font styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {HashRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home.tsx'
+import Home from './Pages/home.tsx'
 import Demo from './Pages/Demo.tsx'
 import Layout from './Components/Layout.tsx'
 import Resume from './Pages/Resume.tsx'
@@ -9,28 +9,23 @@ import Fair from './Pages/Fair.tsx'
 
 
 
-function App() {
-  
+function App() {  
 
   return (
     <>
     <Router>
       <Routes>
         <Route element={<Layout/>}>
-          <Route path="/" element={<Fair />}/>
+          <Route path="/" element={<UnderConstruction />}/>
           <Route path="/home" element={<Home />}/>
           <Route path="/demo" element={<Demo />}/>
           <Route path="/resume" element={<Resume />}/>
           <Route path="/uc" element={<UnderConstruction />}/>
-          <Route path="/fair" element={<Fair />}></Route>
-          
+          <Route path="/fair" element={<Fair />}></Route>          
         </Route>
       </Routes>
-    </Router>
-
-      
-    </>
-    
+    </Router>      
+    </>    
   )
 }
 
--- src/Pages/UnderConstruction.tsx
+++ src/Pages/UnderConstruction.tsx
@@ -8,8 +8,6 @@ function UnderConstruction() {
 
 
     //@ts-expect-error icon prop is not working
-    const myIcon : IconProp = "fa-solid fa-helmet-safety"
-    //@ts-expect-error icon prop is not working
     const iconCog : IconProp = "fa-solid fa-cog"
 
     const [cogSpeed, setCogSpeed] = useState('cog-slow');
@@ -29,29 +27,14 @@ function UnderConstruction() {
     
     return (<>
         <main className="main-content under-construction">
-        <h1><FontAwesomeIcon icon={myIcon}   />Website Under Development</h1>
+        <h1>Website Under Contruction</h1>
         <div className='cogs'>        
         <FontAwesomeIcon className={cogSpeed}  icon={iconCog} size="6x" spin spinReverse/>
-        <FontAwesomeIcon className={cogSpeed}  icon={iconCog} size="3x" spin />
-        
+        <FontAwesomeIcon className={cogSpeed}  icon={iconCog} size="3x" spin />     
 
         </div>
         <button onClick={accelerate}>Click Here to Speed Up Process</button>
-        
-            
-            <h2>
-                <em>Hey there! I'm Oliver, a passionate IT student, and
-                    my portfolio is getting some exciting updates.
-                </em>
-            </h2>
-            <p>Currently coding my next big project...
-                New features loading... 80% complete! Real-time debugging in progress...
-                but the magic is almost ready!
-                <br />
-                Stay tuned something awesome is coming soon!
-                <br />
-                In the meantime, feel free to connect with me on LinkedIn or check out my GitHub for a sneak peek into what I'm working on!
-            </p>
+        <p>On PC? Try moving your mouse over the cogs.</p>
         </main>
 
     </>);

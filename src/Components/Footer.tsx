aa8aa98616918b9037c98152361740aa9190b542
OGxLuxuries <ogluxuries@gmail.com>
2026-02-15 12:51:16 -0500

    slight changes

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import LinkedinBadge from './LinkedinBadge';
 import '../CSS/Footer.css';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <LinkedinBadge />
-      </div>
-    </footer>
+    <></>
   );
 };
 
--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -6,12 +6,7 @@ function Resume() {
       
       <section className="main-content grid-container rounded">
       <h1>Resume</h1>
-      <embed
-    src="http://infolab.stanford.edu/pub/papers/google.pdf#toolbar=0&navpanes=0&scrollbar=0"
-    type="application/pdf"
-    height="100%"
-    width="100%"
-></embed>
+      <p>Coming Soon</p>
       </section>
     </>
   );
